08f32627cdb12b8ad22f8ea5e1a633dd3a746259
rochafialvin <rochafi.dev@gmail.com>
2020-04-15 09:46:18 +0700

    Read and Delete by Id

--- index.js
+++ index.js
@@ -27,35 +27,86 @@ app.get('/', (req, res) => {
 // Read All User
 app.get('/users', async (req, res) => {
 
+   // Akan mencoba menjalankan kode di dalam 'try', jika terjadi masalah akan pindah ke 'catch' 
+   // dan informasi masalahnya akan ada di variable 'err'
    try {
+      // Mencari semua user
       let users = await User.find({})
+      // Kirim hasil pencarian sebagai bentuk respon
       res.send(users)
    
    } catch (err) {
+      // Kirim object 'err' sebagai bentuk respon
       res.send(err)
 
    }
 
 })
 
-// Read One User By Id
-
-// {error : "User dengan id 989999 tidak ditemukan"}
-
 // Create New User
-app.post('/users', async (req, res) => {
+app.post('/users/:id', async (req, res) => {
    // req.body = {username : 'rochafi', name: 'Rochafi', age: 28}
 
    // Create new user
    const user = new User(req.body)
    // Save ke database
    try {
+      // save() digunakan untuk menyimpan user baru ke dalam database
       let result = await user.save()
+      // Result dari proses ini akan dikirim sebagai respon
       res.send(result)
+
    } catch (err) {
       res.send(err)
    }
       
+})
+
+// Read One User By Id
+app.get('/user/:id', async (req, res) => {
+   // Menyimpan id user di variable
+   let _id = req.params.id
+
+   try{
+      // Mencari user berdasarkan id
+      let user = await User.findById(_id)
+
+      // Jika user tidak ditemukan, maka variable 'user' akan kosong 
+      if(!user){
+         // Kirim object berupa pesan error
+         return res.send({ error : `User dengan id ${_id} tidak ditemukan` })
+      }
+
+      // Kirim user sebagai bentu respon
+      res.send(user)
+
+   } catch (err){ // Jika terjadi masalah dalam proses pencarian data
+      res.send(err)
+
+   }
+})
+
+// Update User By Id
+
+// Delete User By Id
+app.delete('/user/:id', async (req, res) => {
+   let _id = req.params.id
+
+   try {
+      let user = await User.findByIdAndDelete(_id)
+
+      if(!user){
+         return res.send({ message: 'User tidak ditemukan di database' })
+      }
+
+      res.send({
+         message: 'User berhasil di hapus', user
+      })
+
+   } catch (err) {
+      res.send(err)
+      
+   }
 })
 
 
